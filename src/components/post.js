c8d5a00f46019dd7d3b670bf7f0960408cd87cb9
Grobla <you@example.com>
2019-11-07 22:23:08 +0100

    java python c++ indicators

--- src/components/post.js
+++ src/components/post.js
@@ -21,13 +21,41 @@ const styles = {
 
         const {classes, post : {title, shortDesc, userHandle, createdAt, userImage, postId, java, python, cpp}} = this.props;
 //https://youtu.be/m_u6P5k0vP0?list=WL&t=18473
+var j_src;
+var p_src;
+var c_src;
+if(java !== "") {
+    j_src = "java_logo.png"
+}
+
+if(python !== "") {
+    p_src = "python_logo.png"
+}
+
+if(cpp !== "") {
+    c_src = "cpp_logo.png"
+}
+
         return (
             <div className="post-margin">
            <Card>
                <CardMedia image={userImage} title="User image" />
                <CardContent>
                    <Typography className="post-title" component={Link} to={`/posts/${postId}`} variant="h6">{title}</Typography>
-                   <div className="post-langs">java,cpp,python</div>
+                   <div className="post-langs">
+                       
+                            
+                        <img draggable="false"  src={j_src} height="24px"/> 
+                        
+                       
+
+                       
+                        <img draggable="false" src={p_src} height="24px"/> 
+
+                       
+                        <img draggable="false" src={c_src} height="24px"/> 
+
+                   </div>
                    <br/> <br/>
                    <Typography variant="body2">{shortDesc}</Typography>
                   
