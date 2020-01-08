7c15e16463f9e002daecff81cd53b03612d57999
Piotr <piotreksl2002@go2.pl>
2020-01-08 20:30:40 +0100

    Updates

---
+++ precache-manifest.3c2ac17eac622324255a47ad19064149.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7f1a3531d6b8916c289f5597ceb2ed6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e959a29832f5e345db69",
+    "url": "/static/css/main.01a16abc.chunk.css"
+  },
+  {
+    "revision": "66bcfc642fe1c6d6b73a",
+    "url": "/static/js/2.79c1afc4.chunk.js"
+  },
+  {
+    "revision": "e959a29832f5e345db69",
+    "url": "/static/js/main.dbe28461.chunk.js"
+  },
+  {
+    "revision": "d8f33b4360261e73aa0b",
+    "url": "/static/js/runtime-main.dc18b3d6.js"
+  }
+]);
